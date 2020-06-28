8a8b46aef0118b927ed52a43b8223333cbbf2bb1
commodore73 <git@c73.me>
2020-06-28 07:15:19 -0700

    draft sample code

---
+++ cstest/Program.cs
@@ -0,0 +1,163 @@
+﻿namespace CommandName
+{
+    using System;
+    using System.IO;
+
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    using McMaster.Extensions.CommandLineUtils; // alternative to abandonware Microsoft.Extensions.CommandLineUtils.
+
+    using Contentstack.Core;
+    using Contentstack.AspNetCore;
+
+    using Deliverystack.Core;
+    using Deliverystack.Core.Models.Repositories;
+    using Deliverystack.Core.Stackcontent.Configuration;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Message($"{typeof(Program)} : Main()", true);
+            CommandLineApplication app = new CommandLineApplication();
+
+            try
+            {
+                app.Description = ".NET Core console app to "; //TODO
+                app.Name = typeof(Program).Namespace;
+                CommandOption help = app.HelpOption("-?|-h|--help");
+                CommandOption dir = app.Option("-d|--d<value>",
+                    "Path to directory containing appconfig.json",
+                    CommandOptionType.SingleValue);
+                app.OnExecute(() =>
+                {
+                    if (help.HasValue())
+                    {
+                        ExitMessage(app, null, ExitCode.HelpRequested);
+                    }
+
+                    string directory = Directory.GetCurrentDirectory();
+
+                    if (dir.HasValue())
+                    {
+                        directory = dir.Value();
+                    }
+
+                    if (!Directory.Exists(directory))
+                    {
+                        ExitMessage(app,
+                            $"{directory} does not exist or is not a subdirectory.",
+                            ExitCode.DirectoryDoesNotExist);
+                    }
+
+                    IConfiguration configuration = new ConfigurationBuilder().AddJsonFile(
+                        $"{directory}\\appsettings.json",
+                        optional: false,
+                        reloadOnChange: true).Build();
+                    Startup startup = new Startup();
+                    ServiceCollection
+                        serviceCollection = new ServiceCollection(); // dependency injection configuration 
+                    startup.ConfigureServices(serviceCollection, configuration); // configuration
+                    IServiceProvider provider = serviceCollection.BuildServiceProvider(); // caution: may be disposable 
+                    ContentstackClient client = provider.GetService<ContentstackClient>();
+//                    client.SerializerSettings.Converters.Add(); //TODO:
+                    IRepository repository = provider.GetService<IRepository>();
+                    Console.WriteLine(Environment.NewLine);
+                    Message(typeof(ThingDoer) + ".DoThing()", true);
+                    new ThingDoer().DoThing(client, repository as ContentstackRepository);
+                    Message(typeof(ThingDoer) + ".DoThing() complete.");
+                    Console.WriteLine(Environment.NewLine);
+                });
+
+                app.Execute(args);
+
+            }
+            catch (Exception ex)
+            {
+                new Instrument().Exception(ex, ex, "exception in Main()");
+///                ExitMessage(app, $"{ex} : {ex.Message}", ExitCode.Exception, ex);
+                ExitMessage(app, $"{ex} : {ex.Message}", ExitCode.Exception);
+            }
+        }
+        
+        private enum ExitCode
+        {
+            Success,
+            Exception,
+            HelpRequested,
+            DirectoryDoesNotExist
+        }
+        
+        private class Startup
+        {
+            public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+            {
+                services.AddContentstack(configuration);
+                Root.Core.Models.Configuration.IServiceCollectionExtensions.AddSerializationConfigurator(services, configuration);
+                services.AddRepository(configuration);
+            }
+        }
+
+        private static void ExitMessage(CommandLineApplication app,
+            string message,
+            Exception ex = null)
+        {
+            ExitMessage(app, message, null, ex);
+        }
+
+        private static void ExitMessage(CommandLineApplication app,
+            string message,
+            ExitCode? exitCode = null,
+            Exception ex = null)
+        {
+            if (!String.IsNullOrEmpty(message))
+            {
+                Console.WriteLine(Environment.NewLine + app.Name + " : " + message + Environment.NewLine);
+            }
+            else if (ex != null)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            if (exitCode != null)
+            {
+                app.ShowHelp();
+            }
+
+            if (ex != null)
+            {
+                Console.WriteLine(Environment.NewLine + ex.GetType() + " : " + ex.Message);// + " : " + ex.Message);
+
+                while (ex.InnerException != null)
+                {
+                    ex = ex.InnerException;
+                    Console.WriteLine(Environment.NewLine + ex.GetType() + " : " + ex.Message);
+                }
+            }
+            
+            if (exitCode != null)
+            {
+                Environment.Exit((int) exitCode);
+            }
+        }
+
+        private static void Message(string message, bool addLine = false)
+        {
+            if (!string.IsNullOrEmpty(message))
+            {
+                Console.WriteLine($"{DateTime.Now:h:mm:ss.fffffff} : {typeof(Program)} : " + message);
+            }
+
+            if (addLine)
+            {
+                Line();
+            }
+        }
+
+        private static void Line()
+        {
+            Message("========================================================================================");
+        }
+    }
+}
---
+++ cstest/ThingDoer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace CommandName
+{
+    using System;
+    using System.Diagnostics;
+    using System.Net.Http;
+    using System.Runtime.CompilerServices;
+    using Contentstack.Core;
+    using Deliverystack.Core.Models.Entries;
+    using Deliverystack.Core.Models.Repositories;
+    using Root.Core.Models.Entries.Author;
+    using Root.Core.Models.Entries.Resource;
+    using Root.Core.Models.Fields;
+    using Root.Core.Models.GlobalFields.Elements;
+
+    public class ThingDoer
+    {
+        private void McHammer()
+        {
+            double i = 0;
+            int limit = 1000;
+            DateTime start = DateTime.Now;
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                do
+                {
+                    try
+                    {
+                        if (i % 100 == 0)
+                        {
+                            Console.Write("#");
+                        }
+
+                        string discard = httpClient.GetStringAsync("https://localhost:5001/").Result;
+                    }
+                    catch (HttpRequestException ex)
+                    {
+                        Console.WriteLine(ex.GetType() + " : " + ex.Message);
+                        // break on first error
+                        limit = 0;
+                    }
+                } while (i++ < limit);
+            }
+
+            TimeSpan timeSpan = new TimeSpan(DateTime.Now.Ticks - start.Ticks);
+            double ms = timeSpan.Ticks / 10000;
+            double msPerRequest = ms / --i;
+            double requestPerSec = i / ms;
+            Console.WriteLine($"{this} : {i} requests in {ms}ms ({Math.Round(msPerRequest * 1000) / 1000} ~ms/request; ~{Math.Round(requestPerSec * 1000)} request/sec)");
+        }
+
+        public void DoThing(ContentstackClient client, ContentstackRepository repository)
+        {
+            if (true)
+            {
+                McHammer();
+            }
+            
+            ResourceEntry entry = repository.Get<ResourceEntry>("blt9bfc410b2e10a6f0");
+
+            foreach (var thing in repository.GetChildren(entry.EntryUid, entry.ContentTypeUid))
+            {
+                Console.WriteLine("CHILD: " + thing);
+            }
+
+
+
+            Console.WriteLine(repository.GetChildIdentifiers(entry.EntryUid, entry.ContentTypeUid));
+
+            foreach (Tuple<string, string> childIds in repository.GetChildIdentifiers(entry.EntryUid,
+                entry.ContentTypeUid))
+            {
+                EntryPointer child = repository.Get<EntryPointer>(childIds.Item1, childIds.Item2);
+                Console.WriteLine("Child: " + child.Url);
+            }
+            
+            foreach (var block in entry.Elements.Members)
+            {
+                switch (block.ElementType)
+                {
+                    case ElementType.CodeBlock:
+                        Console.WriteLine("CodeBlock: " + ((CodeBlockElement) block).Code);
+                        break;
+                    case ElementType.CodeLink:
+                        Console.WriteLine("CodeLink: " + ((CodeLinkElement) block).Url);
+                        break;
+                    case ElementType.Download:
+                        Console.WriteLine("Download: " + ((DownloadElement) block).Asset.Value.Url);
+                        break;
+                    case ElementType.Image:
+                        Console.WriteLine("Image: " + ((ImageElement) block).Asset.Value.Url);
+                        break;
+                    case ElementType.Markdown:
+                        Console.WriteLine("Markdown: " + ((MarkdownElement) block).Markdown.Markup);
+                        break;
+                    case ElementType.RichText:
+                        Console.WriteLine("RichText: " + ((MarkupElement) block).RichText.Value);
+                        break;
+                    default:
+                        Trace.Assert(false, 
+                        String.Format("{0} {1} not recognized", block.ElementType.GetType(), block.ElementType));
+                        break;
+                }
+            }
+
+            foreach (ReferenceField<AuthorEntry> field in entry.PageData.Authors)
+            {
+                Console.WriteLine("Author: "+ field.Get().Name);
+            }
+        }
+    }
+}
